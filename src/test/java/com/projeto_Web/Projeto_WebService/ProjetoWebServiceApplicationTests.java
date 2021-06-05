39fab2309b2b24c9253864ae0055828cd2cd3a7c
Ronaldo Adriano Henrique <ronaldohenrique374@gmail.com>
2021-06-05 16:29:36 -0300

    Projeto Criado

---
+++ src/test/java/com/projeto_Web/Projeto_WebService/ProjetoWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_Web.Projeto_WebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
